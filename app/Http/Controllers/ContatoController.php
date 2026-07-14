238f10fab8f2d151dc3a38ca15f995e034525164
João Pedro de Oliveira Paulino <joaopoliveirapaulino@gmail.com>
2026-07-14 14:32:01 -0300

    Aula - Formulario via POST

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 class ContatoController extends Controller
 {
     public function contato() {
 
-        var_dump($_GET);
+        var_dump($_POST);
         return view('site.contato');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobre-nos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function(){return 'Login';})->name('site.login');
 
 Route::prefix('/app')->group(function(){
